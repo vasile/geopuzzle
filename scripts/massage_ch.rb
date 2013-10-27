69f4b0721c5fc18e6d3381220524560beea7e527
Vasile Cotovanu <vasile.cotovanu@local.ch>
2013-10-27 22:30:40 +0100

    Round to 6 decimals

--- scripts/massage_ch.rb
+++ scripts/massage_ch.rb
@@ -9,6 +9,18 @@ new_json = {
   'features' => []
 }
 
+def get_group_coordinates(groups)
+  groups.each_with_index do |coords_group, k|
+    new_coords = []
+    coords_group.each do |coords|
+      new_coords.push([coords[0].round(6), coords[1].round(6)])
+    end
+    groups[k] = new_coords
+  end
+  
+  return groups
+end
+
 json = JSON.parse(File.open(json_in, "r").read)
 json['features'].each do |f|
   new_properties = {
@@ -16,6 +28,16 @@ json['features'].each do |f|
     'code' => f['properties']['code'].nil? ? f['properties']['KURZ'] : f['properties']['code'],
   }
 
+  if f['geometry']['type'] == 'Polygon'
+    f['geometry']['coordinates'] = get_group_coordinates(f['geometry']['coordinates'])
+  end
+  
+  if f['geometry']['type'] == 'MultiPolygon'
+    f['geometry']['coordinates'].each_with_index do |polygon_coordinates, k|
+      polygon_coordinates = get_group_coordinates(polygon_coordinates)
+    end
+  end
+  
   new_json['features'].push({
     'type' => 'Feature',
     'properties' => new_properties,
